0522aa29625b9e86c70e71908700ee55fa5bd0d9
Gunlords <tur68285@temple.edu>
2025-04-03 12:01:46 -0400

    button, binder and service control setup

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,21 +1,64 @@
 package edu.temple.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+
+import android.content.*
+import android.os.*
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var timerService: TimerService.TimerBinder? = null
+    private var isBound = false
+
+    private val handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            findViewById<TextView>(R.id.textView).text = msg.what.toString()
+        }
+    }
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerService = service as TimerService.TimerBinder
+            isBound = true
+            timerService?.setHandler(handler)
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isBound = false
+            timerService = null
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val intent = Intent(this, TimerService::class.java)
+        bindService(intent, connection, Context.BIND_AUTO_CREATE)
+
         findViewById<Button>(R.id.startButton).setOnClickListener {
-
+            handleStartButtonClick()
         }
-        
-        findViewById<Button>(R.id.stopButton).setOnClickListener {
 
+        findViewById<Button>(R.id.stopButton).setOnClickListener {
+            handleStopButtonClick()
+        }
+    }
+
+    private fun handleStartButtonClick() {
+        if (isBound) {
+            if (!timerService!!.isRunning) {
+                timerService?.start(10)
+            } else {
+                timerService?.pause()
+            }
+        }
+    }
+
+    private fun handleStopButtonClick() {
+        if (isBound) {
+            timerService?.stop()
         }
     }
 }
